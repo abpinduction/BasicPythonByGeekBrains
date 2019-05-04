a89a82a04c7bbd72bc89fa32996333a177d189ec
Alexander Bal <abpinduction@yandex.ru>
2019-05-04 15:35:25 +0300

    Симулятор банкомата

--- 04hard.py
+++ 04hard.py
@@ -26,12 +26,12 @@ def is_pin_valid(person, pin_code):
     """
     chance = 3
     while person['pin'] != pin_code:
-        print(f'Пин код не верен. Осталось {chance} попыток')
+        print(f'Будьте внимательны. Осталось {chance} попытки')
         chance -= 1
         if chance < 0:
             break
         try:
-            pin_code = int(input('Введите пин код'))
+            pin_code = int(input('Введите пин код: '))
         except ValueError:
             print('Пин код - 4 цифры без букв и спец символов!')
     return True
@@ -55,14 +55,17 @@ def withdraw_money(person, money):
 
 
 def process_user_choice(choice, person):
-    # проверка на ввод не правильных цифр.
+    # проверка на ввод неправильных цифр.
     while choice > 3 or choice < 1:
-        choice = int(input('Введите номер операции согласно списка меню:'))
+        choice = int(input('Введите номер операции согласно списка меню: '))
     if choice == 1:
         print(f'На Вашем счету: {check_account(person)}')
     if choice == 2:
-        take_money = float(input('Сумма к снятию: '))
-        print(withdraw_money(person, take_money))
+        try:
+            take_money = float(input('Сумма к снятию: '))
+            print(withdraw_money(person, take_money))
+        except ValueError:
+            print('Неправильно набрана сумма')
 
 
 def start():
@@ -79,22 +82,22 @@ def start():
     person = get_person_by_card(card_number)
     if person and is_pin_valid(person, pin_code):
         while True:
-            choice = int(input('Выберите пункт:\n'
-                               '1. Проверить баланс\n'
-                               '2. Снять деньги\n'
-                               '3. Выход\n'
-                               '---------------------\n'
-                               'Ваш выбор: '))
-            if choice == 3:
-                break
-            process_user_choice(choice, person)
-
-
-#            except ValueError:
-#                print('Введите номер операции согласно списка меню: ')
-# данный блок становится лишним при ранней проверке ввода значений
-# else:
-# print('Номер карты или пин код введены не верно!')
+            try:
+                choice = int(input('Выберите пункт:\n'
+                                   '1. Проверить баланс\n'
+                                   '2. Снять деньги\n'
+                                   '3. Выход\n'
+                                   '---------------------\n'
+                                   'Ваш выбор: '))
+                if choice == 3:
+                    print('HOPE TO SEE YOU SOON!\n'
+                          'GOOD BUY, SIR!')
+                    break
+                process_user_choice(choice, person)
+            except ValueError:
+                print('Введите номер операции согласно списка меню: ')
+    else:
+        print('Номер карты или пин код введены не верно!')
 
 
 start()
