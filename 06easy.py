81292c08be53cde706b93a8084d269413344f294
Alexander Bal <abpinduction@yandex.ru>
2019-05-11 20:44:55 +0300

    ООП. Создание классов автомобилей. Наследование, переопределение функций, полиморфизм.

---
+++ 06easy.py
@@ -0,0 +1,158 @@
+# Задача - 1
+# Опишите несколько классов TownCar, SportCar, WorkCar, PoliceCar
+# У каждого класса должны быть следующие аттрибуты:
+# speed, color, name, is_police - Булево значение.
+# А так же несколько методов: go, stop, turn(direction) - которые должны сообщать,
+#  о том что машина поехала, остановилась, повернула(куда)
+
+
+class TownCar:
+    def __init__(self, speed, name):
+        self.speed = speed
+        self.name = name
+        self.is_police = False
+
+    def go(self):
+        print('Машина поехала')
+
+    def turn(self, direction):
+        print('Машина повернула на {}'.format(direction))
+
+    def stop(self):
+        print('Машина остановилась')
+
+
+class SportCar(TownCar):
+    pass
+
+
+class WorkCar(TownCar):
+    pass
+
+
+class PoliceCar(TownCar):
+    super().__init__(is_police=True)
+
+# Вариант преподавателя
+
+# class TownCar:
+#
+#     def __init__(self, speed, color, name, is_police=False):
+#         self.speed = speed
+#         self.color = color
+#         self.name = name
+#         self.is_police = is_police
+#
+#     def go(self):
+#         print('{} is going!'.format(self.name))
+#
+#     def stop(self):
+#         print('{} is stoping!'.format(self.name))
+#
+#     def turn(self, direction):
+#         print('{} is turning to {}!'.format(self.name, direction))
+#
+#
+# class SportCar:
+#     def __init__(self, speed, color, name, is_police=False):
+#         self.speed = speed
+#         self.color = color
+#         self.name = name
+#         self.is_police = is_police
+#
+#     def go(self):
+#         print('{} is going!'.format(self.name))
+#
+#     def stop(self):
+#         print('{} is stoping!'.format(self.name))
+#
+#     def turn(self, direction):
+#         print('{} is turning to {}!'.format(self.name, direction))
+#
+#
+# class WorkCar:
+#     def __init__(self, speed, color, name, is_police=False):
+#         self.speed = speed
+#         self.color = color
+#         self.name = name
+#         self.is_police = is_police
+#
+#     def go(self):
+#         print('{} is going!'.format(self.name))
+#
+#     def stop(self):
+#         print('{} is stoping!'.format(self.name))
+#
+#     def turn(self, direction):
+#         print('{} is turning to {}!'.format(self.name, direction))
+#
+#
+# class PoliceCar:
+#     def __init__(self, speed, color, name):
+#         self.speed = speed
+#         self.color = color
+#         self.name = name
+#         self.is_police = True
+#
+#     def go(self):
+#         print('{} is going!'.format(self.name))
+#
+#     def stop(self):
+#         print('{} is stoping!'.format(self.name))
+#
+#     def turn(self, direction):
+#         print('{} is turning to {}!'.format(self.name, direction))
+
+
+# Задача - 2
+# Посмотрите на задачу-1 подумайте как выделить общие признаки классов
+# в родительский и остальные просто наследовать от него.
+
+# В моем исполнении уже ничего делать не надо было
+
+# Вариант преподавателя
+#
+# class Car:
+#     def __init__(self, speed, color, name):
+#         self.speed = speed
+#         self.color = color
+#         self.name = name
+#         self.is_police = False
+#
+#     def go(self):
+#         print('{} is going!'.format(self.name))
+#
+#     def stop(self):
+#         print('{} is stoping!'.format(self.name))
+#
+#     def turn(self, direction):
+#         print('{} is turning to {}!'.format(self.name, direction))
+#
+#
+# class TownCar(Car):
+#     pass
+#
+#
+# class SportCar(Car):
+#     def __init__(self, speed, color, name, has_turbo=True):
+#         super().__init__(speed, color, name)
+#         self.has_turbo = has_turbo
+#
+#
+# class WorkCar(Car):
+#     pass
+#
+#
+# class PoliceCar(Car):
+#     def __init__(self, speed, color, name):
+#         super().__init__(speed, color, name)
+#         self.is_police = True
+#
+#
+# sportCar = SportCar(240, 'Красная', 'Спортивная машина')
+# townCar = TownCar(140, 'Серая', 'Городская машина')
+# workCar = WorkCar(90, 'Желтая', 'Рабочая машина')
+# policeCar = PoliceCar(210, 'Синяя', 'Полицейская машина')
+#
+#
+# print(policeCar.is_police)
